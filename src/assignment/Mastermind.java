43ec93ac1d520f2497084154b07b9c28cc13b3e0
michellescott <mscott40@gmail.com>
2016-10-20 10:46:12 -0400

    Initial commit

---
+++ src/assignment/Mastermind.java
@@ -0,0 +1,176 @@
+package assignment;
+
+import java.util.HashSet;
+import java.util.Set;
+import java.util.Random;
+import java.util.Scanner;
+
+/**
+ * Mastermind is a code-breaking game. The codebreaker tries to guess the pattern (called code in this class),
+ * in both order and color, within ten turns. Each guess is made by entering a series of characters where each
+ * character represents a color.
+ *
+ * The code-maker (in this case the computer) provides feedback by returning a count of the number of right
+ * and wrong colors in the pattern. An element is right if it is correct in both color and position.
+ * An element is wrong if it is the correct color code peg placed in the wrong position.
+ * If there are duplicate colours in the guess, they cannot all be counted in the feedback unless
+ * they correspond to the same number of duplicate colours in the hidden code.
+ *
+ * I've implemented most of it correctly, but there are a few bugs that still need to be fixed.
+ * Write test cases / play the game / force results in the game to find and resolve these bugs.
+ */
+public class Mastermind {
+
+    private static final int CODE_LENGTH = 4;
+    private static final int TOTAL_GUESSES = 10;
+    private static final String AVAILABLE_COLORS = "BGPRWY";
+    private static final int NUM_COLORS = AVAILABLE_COLORS.length();
+
+    private char[] code;
+    private char[] guessChars;
+    private int guesses;
+    private Random rand;
+    private Scanner scanner;
+    private Feedback fb;
+    private Set<Character> remaining;
+
+    public Mastermind() {
+        scanner = new Scanner(System.in);
+        code = new char[CODE_LENGTH];
+        guessChars = new char[CODE_LENGTH];
+        fb = new Feedback();
+        remaining = new HashSet<Character>();
+        guesses = TOTAL_GUESSES;
+        rand = new Random();
+        generateCode();
+        System.out.println("Welcome to Mastermind! You get 10 tries to guess the code.");
+        System.out.println("There are 6 possible colors in the code red(R), blue(B), green(G),\n" +
+                "yellow(Y), white(W), and purple(P). Good luck!");
+        System.out.println("Type QUIT at any point to end a game.");
+    }
+
+    /**
+     * Generate a new code and reset the number of guesses.
+     */
+    private void generateCode() {
+        for (int i = 0; i < CODE_LENGTH; i++) {
+            int index = rand.nextInt(NUM_COLORS);
+            code[i] = AVAILABLE_COLORS.charAt(index);
+        }
+        guesses = TOTAL_GUESSES;
+        fb.reset();
+    }
+
+    /**
+     * Determine the feedback for a given pattern.
+     */
+    private void testPattern(String guess) throws Exception {
+        if (guess.length() != CODE_LENGTH) {
+            throw new Exception("Invalid guess: " + guess);
+        }
+        // End the game
+        if (guess.equals("QUIT")) {
+            guesses = 0;
+            return;
+        }
+        // Clear old data
+        fb.reset();
+        remaining.clear();
+        // Iterate through the guess characters.
+        for (int i = 0; i < CODE_LENGTH; i++) {
+            char curr = guess.charAt(i);
+            // If a character is invalid, make the user guess again.
+            if (AVAILABLE_COLORS.indexOf(curr) == -1) {
+                throw new Exception("Invalid character: " + curr);
+            }
+            // If the character matches, add one to the right count.
+            if (curr == code[i]) {
+                fb.right();
+            } else {
+                remaining.add(code[i]);
+                guessChars[i] = curr;
+            }
+        }
+        for (int i = 0; i < CODE_LENGTH; i++) {
+            char curr = guessChars[i];
+            // If a character is the right color, but in the wrong place, add one to the wrong count.
+            if (remaining.contains(curr)) {
+                fb.wrong();
+                remaining.remove(Character.valueOf(curr));
+            }
+        }
+    }
+
+    private boolean playTurn(int guessNumber) {
+        // Get pattern guess from user
+        System.out.print(String.format("%d. Enter a guess: ", guessNumber));
+        String guess = scanner.next().trim().toUpperCase();
+        String result;
+        try {
+            testPattern(guess);
+            guesses--;
+            result = fb.toString();
+        } catch (Exception e) {
+            result = e.getMessage();
+        }
+
+        System.out.println(result);
+        return fb.correct();
+    }
+
+    private String codeString() {
+        return new String(code);
+    }
+
+    public boolean playGame() {
+        while(guesses > 0) {
+            if (playTurn(TOTAL_GUESSES + 1 - guesses)) {
+                System.out.println("Correct!");
+                break;
+            }
+        }
+        System.out.println("The code was " + codeString());
+        generateCode();
+        System.out.print("Play again? Enter \'yes\' to start a new game. ");
+        String answer = scanner.next().trim().toUpperCase();
+        return answer.equals("YES");
+    }
+
+    private class Feedback {
+        private int right = 0;
+        private int wrong = 0;
+
+        public void reset() {
+            right = 0;
+            wrong = 0;
+        }
+
+        public void right() {
+            this.right++;
+        }
+
+        public void wrong() {
+            this.wrong++;
+        }
+
+        public boolean correct() {
+            return right == 4;
+        }
+
+        @Override
+        public String toString() {
+            return String.format("Right=%d Wrong=%d", right, wrong);
+        }
+    }
+
+    public static void main(String[] args) {
+        Mastermind m = new Mastermind();
+        int i = 1;
+        final String ROUND = "Round %d";
+        System.out.println(String.format(ROUND, i++));
+        while(m.playGame()) {
+            System.out.println(String.format(ROUND, i++));
+        }
+    }
+
+}
---
+++ src/assignment/StaticMethods.java
@@ -0,0 +1,125 @@
+package assignment;
+
+
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
+
+/**
+ * All of these methods have bugs. Use tests to find the bugs and fix them.
+ */
+public class StaticMethods {
+
+    /**
+     * This method should implement the binary search algorithm
+     * for a list of sorted integers and some integer value.
+     * This method should return true if value is one of the
+     * values in arr.
+     *
+     * A binary search begins by comparing the middle element of the
+     * array with the target value. If the target value matches the
+     * middle element, its position in the array is returned. If
+     * the target value is less than or greater than the middle element,
+     * the search continues in the lower or upper half of the array,
+     * respectively, eliminating the other half from consideration.
+     */
+    public static boolean binarySearch(int[] arr, int value) {
+        if(arr == null) {
+            return false;
+        }
+        int begin = 0;
+        int end = arr.length;
+        int mid;
+        while (begin <= end) {
+            mid = getMiddleIndex(begin, end);
+            if (arr[mid] == value) {
+                return true;
+            } else if(arr[mid] > value) {
+                begin = mid + 1;
+            } else { // arr[mid] < value
+                end = mid - 1;
+            }
+        }
+        return false;
+    }
+
+    // Helper method for the binarySearch algorithm. This returns
+    // the middle index between begin and end.
+    private static int getMiddleIndex(int begin, int end) {
+        return (begin + end)/2;
+    }
+
+    /**
+     * This method takes an array of strings. Each adjacent pair of strings makes
+     * a key-value pair. If there are an odd number of strings, the last string is ignored.
+     * For example {"a", "b", "c", "d"} should create the map {a:b, c:d} using the format key:value.
+     */
+    public static Map<String, String> stringMap(String[] arr) {
+        HashMap<String, String> map = new HashMap<String, String>();
+        if (arr == null) {
+            return map;
+        }
+        for (int i = 0; i < arr.length - 2; i++) {
+            map.put(arr[i], arr[i+1]);
+        }
+        return map;
+    }
+
+    /**
+     * This method takes in a 3x3 array of characters and a character called player (which should be an 'X' or 'O').
+     * Returns true if player has won the game of tic-tac-toe. Winning means that the player has three in a row.
+     *
+     *  X | O | X
+     *  O | X |
+     *  X |   | O    X wins!
+     */
+    public static boolean didWin(char[][] game, char player) {
+        if (game == null) {
+            return false;
+        }
+        boolean hasMiddle = game[1][1] == 'X';
+        for (int i = 0, j = 2; i <= 2; i++, j--) {
+            // Check for a vertical win
+            if (player == game[0][i] && player == game[1][i] && player == game[2][i]) {
+                return true;
+            }
+            // Check for a horizontal win
+            if (player == game[i][0] && player == game[i][1] && player == game[i][2]) {
+                return true;
+            }
+            // Check along the diagonals
+            if (hasMiddle) {
+                if (game[0][i] == player && game[2][j] == player) {
+                    return true;
+                } else {
+                    return false;
+                }
+            }
+        }
+
+        return false;
+    }
+
+    /**
+     * This method computes the sum of the unique digits in a positive number.
+     * sumDigits(12) => 3
+     * sumDigits(121) => 3
+     * sumDigits(1421) => 7
+     */
+    public static int sumDigits(int value) {
+        if (value < 0) {
+            return -1;
+        }
+        Set<Integer> digits = new HashSet<Integer>();
+        while (value != 0) {
+            digits.add(value % 2);
+            value /= 10;
+        }
+        int sum = 0;
+        for (int i : digits) {
+            sum += 1;
+        }
+        return sum;
+    }
+}
